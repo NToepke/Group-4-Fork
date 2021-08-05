e011e797d167626b7f345a7a7fe069fe904ad587
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2021-08-05 08:51:25 -0500

    Test find_id_from_login
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- workers/gitlab_issues_worker/gitlab_issues_worker.py
+++ workers/gitlab_issues_worker/gitlab_issues_worker.py
@@ -62,6 +62,13 @@ class GitLabIssuesWorker(WorkerGitInterfaceable):
         update_col_map = {'issue_state': 'state'}
         duplicate_col_map = {'gh_issue_id': 'id'}
 
+        #test find_id_from_login
+        sean_cntrb_id = find_id_from_login("sgoggins")
+        isaac_cntrb_id = find_id_from_login("IsaacMilarky")
+        andrew_cntrb_id = find_id_from_login("ABrain7710")
+
+        self.logger.info(f"Sean: {sean_cntrb_id}    Isaac: {isaac_cntrb_id}     Andrew: {andrew_cntrb_id}")
+
         #list to hold issues needing insertion
         issues = self.paginate(gitlab_issues_url, duplicate_col_map, update_col_map, table, table_pkey,
             'WHERE repo_id = {}'.format(repo_id), platform="gitlab")
