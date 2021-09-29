741400ab47774fd8d1788df27a58efbe884589f0
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2021-09-29 12:44:07 -0500

    Fix up loging for removed data
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- workers/worker_persistance.py
+++ workers/worker_persistance.py
@@ -1005,29 +1005,31 @@ class Persistant():
             all_data = query.join(source_table, eval_value).all()
             self.logger.debug(f"All data len: {len(all_data)}")
 
-
-
-            data_removed_condition_string_list = [
-                f"table.c['{table_column}'] != source_table.c['{source_column}']"
-                for table_column, source_column in zip(
-                    augur_merge_fields, gh_merge_fields
-                )
-            ]
-
-            removed_data_eval = eval(
-                ' and '.join(data_removed_condition_string_list)
-            )
-
-            removed_data = query.join(source_table, removed_data_eval).all()
-
-            self.logger.debug(f"Removed data len: {len(removed_data)}")
-            self.logger.debug(f"Removed data: {removed_data}")
-
-
-            
-
             source_pk = pd.DataFrame(all_data, columns=source_pk_columns)
             self.logger.debug(f"source_pk before _eval_json_columns len: {len(source_pk)}")
+
+            removed_data = pd.DataFrame(
+                session.query(
+                    table.c[list(table.primary_key)[0].name],
+                    source_table
+                ).join(
+                    source_table,
+                    eval(
+                        ' and '.join(
+                            [
+                                f"table.c['{table_column}'] != source_table.c['{source_column}']"
+                                for table_column, source_column in zip(
+                                    augur_merge_fields, gh_merge_fields
+                                )
+                            ]
+                        )
+                    )
+                ).all(), columns=source_pk_columns  # gh_merge_fields
+            )
+
+            self.logger.debug(f"Removed data len: {len(removed_data)}")
+            self.logger.debug(f"Removed data: {removed_data.to_string()}")
+
 
 
 
